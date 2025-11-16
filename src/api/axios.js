dd10e97420048cabe1449c2de1c91dc68c548220
Victoria Galanopoulou <victoria.galanopoulou@gmail.com>
2025-11-16 09:56:46 +0200

    Changes of ports to match with the backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
 });
 
 api.interceptors.request.use((config) => {
